ce4669f80d3fed0e777234425e075074d1c2e3bc
benji694 <bbouillard@edenschool.fr>
2022-03-15 14:25:08 +0100

    function passing the test

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-alert('hello');
+export const pascalToSnake = (str) => {
+    if(typeof str != 'string'){
+        return str.toString();
+    }
+    else{
+        let array = str.split('');
+        let caractèreSpeciaux = ['-', '_', "'", '@', '+', '-', '(', ')', '#', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] == array[i].toUpperCase() && caractèreSpeciaux.indexOf(array[i]) == -1) {
+                if(i == 0){
+                    array[i] = array[i].toLowerCase();
+                }
+                else{
+                    array[i] = '_' + array[i].toLowerCase();
+                }
+            }
+        }
+        let result = array.join('');
+        return result;
+    }
+}
--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
-var assert = require('assert');
+import assert from 'assert';
+import { pascalToSnake } from '../js/app.js'
+
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
@@ -13,19 +15,19 @@ describe('str', function () {
     it('should return test_controller', function () {
       assert.equal(pascalToSnake("TestController"), "test_controller");
     });
-    it('should return string in snake_case', function () {
+    it('should return movie_and_book', function () {
       assert.equal(pascalToSnake("MovieAndBook"), "movie_and_book");
     });
-    it('should return string in snake_case', function () {
+    it('should return app7_test', function () {
       assert.equal(pascalToSnake("App7Test"), "app7_test");
     });
-    it('should return string in snake_case', function () {
+    it('should return home', function () {
       assert.equal(pascalToSnake("Home"), "home");
     });
-    it('should return string in snake_case', function () {
+    it('should return play-movie', function () {
       assert.equal(pascalToSnake("play-movie"), "play-movie");
     });
-    it('should return string in snake_case', function () {
+    it('should return "1"', function () {
       assert.equal(pascalToSnake(1), "1");
     });
   });
